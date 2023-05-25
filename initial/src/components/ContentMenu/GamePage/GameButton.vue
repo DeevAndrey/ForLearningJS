2785293e5116047e391a98f7040c05c2ee46f3c6
Ivanov Ivan <deev.andre@mail.ru>
2023-05-25 17:53:12 +0700

    добавлен новый компонент, используется как нопка для игрового пространства

---
+++ initial/src/components/ContentMenu/GamePage/GameButton.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class = "root">
+      <button v-on:click=" NewState() ">{{state}}</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'GameButton',
+    props:{
+        player:{
+            type: Boolean,
+
+        }
+    },
+    data(){
+        return{
+            state: null
+        }
+    },
+    methods: 
+    {
+        NewState()
+        {
+            if (this.player == true && this.state == null)
+            {
+                this.state = 'O'
+                this.$emit('sendPlayer',this.player)
+            }
+            else if (this.player == false && this.state == null)
+            {
+                this.state = 'X' 
+                this.$emit('sendPlayer',this.player)
+            }
+        }
+    }
+}
+
+</script>
+
+<style>
+button{
+    height: 99%;
+    width: 99%; 
+    font-size: 46px;
+}
+
+</style>
