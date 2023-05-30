3d8202fa5d5e0fd7258ea36b77bd99c5bcab0595
Ivanov Ivan <deev.andre@mail.ru>
2023-05-31 03:21:16 +0700

    Добавлена переменная winner, popupvisible и др. Также написал методы для отслеживания победителя.

--- initial/src/components/ContentMenu/GamePage/GameField.vue
+++ initial/src/components/ContentMenu/GamePage/GameField.vue
@@ -1,65 +1,118 @@
 <template>
     <div class = "root">
         <div class ="field">
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-            <GameButton :player="player" 
-            @click=" ChangePlayer()"
-            @sendPlayer="showPlayer"/>
-
+            
+            <GameButton v-for="button in GameButtons"
+            :key="button.id"
+            :player="player"
+            :PopUpVisible="PopUpVisible"
+            :button_data="button"
+            @sendPlayer="showPlayer"
+            @buttonState="buttonState"
+            />
         </div>
+        <g_PopUp class="modal" 
+        v-if="PopUpVisible"
+        @closePop = "closePopUp()"/>
     </div>
 </template>
 
 <script>
 
 import GameButton from './GameButton.vue';
+import g_PopUp from './Game_PopUp/G_PopUp.vue';
+
 export default{
     name: 'GameXO', 
     data()
     {
-        return{player: true}
+        return{
+            PopUpVisible: false,
+            Winner:
+            [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6]
+            ],
+            GameButtons:
+            [
+            {
+                id: 0,
+                state: null
+            },
+            {
+                id: 1,
+                state: null
+            },
+            {
+                id: 2,
+                state: null
+            },
+            {
+                id: 3,
+                state: null
+            },
+            {
+                id: 4,
+                state: null
+            },
+            {
+                id: 5,
+                state: null
+            },
+            {
+                id: 6,
+                state: null
+            },
+            {
+                id: 7,
+                state: null
+            },
+            {
+                id: 8,
+                state: null
+            }
+            ],
+            player: true}
     } ,
     components:
     {
-        GameButton
+        GameButton,
+        g_PopUp
     },
     methods: 
     {
         showPlayer(data)
         {   
-            console.log(this.player)
-            if (data == true)
-            {
-                
-                this.player = false
+            this.player = data == true? false:true
+
+        },
+        buttonState(data)
+        {
+            this.GameButtons[data.id].state =data.state
+            this.CheckWinner()  
+        },
+        CheckWinner()
+        {
+            for (let i=0;i<8;i++)
+            {   
+                if (this.GameButtons[this.Winner[i][0]].state == this.GameButtons[this.Winner[i][1]].state &&  
+                this.GameButtons[this.Winner[i][0]].state == this.GameButtons[this.Winner[i][2]].state && 
+                this.GameButtons[this.Winner[i][0]].state != null)
+                {
+                    console.log('Победил ', this.GameButtons[this.Winner[i][0]].state)
+                    this.PopUpVisible = true
+                }
             }
-            else
-            {
-                this.player = true
-            }
+        },
+        closePopUp()
+        {
+            this.PopUpVisible = false
         }
     }
 
@@ -68,7 +121,27 @@ export default{
 </script>
 
 <style>
-
+.root
+{
+    height: 99%;
+}
+.modal
+{
+  color: #ffffff;
+  font-family: 'Roboto', sans-serif;
+  position: fixed;
+  left:30%;
+  top:70%;
+  width: 100%;
+  height: 15%;
+  z-index: 2;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(255, 0, 0, 0.904);
+  opacity: 100%;
+  transition: opacity .4s, visibility .4s;
+}
 .field{
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
