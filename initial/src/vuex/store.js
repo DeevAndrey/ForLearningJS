50f17ed6d9137811efbe36ac1fc1b17dd92b85c5
Ivanov Ivan <deev.andre@mail.ru>
2023-06-25 04:21:46 +0700

    расширил функционал локального хранилища

--- initial/src/vuex/store.js
+++ initial/src/vuex/store.js
@@ -6,11 +6,18 @@ Vue.use(Vuex);
 
 let store = new Vuex.Store({
     state:{
-        buttons:[]
+        buttons:[],
+        users:[]
     },
     mutations:{
         SET_BUTTONS_TO_STATE: (state,buttons)=>{
             state.buttons = buttons
+        },
+        SET_USERS_TO_STATE: (state,users)=>{
+            state.users = users
+        },
+        SET_USER:(state,user)=>{
+            state.users.push(user)
         }
     },
     actions:{
@@ -27,11 +34,31 @@ let store = new Vuex.Store({
                 console.log(error)
                 return error
             })
+        },
+        GET_USERS_API({commit}){
+            return axios('http://localhost:3000/Users', {
+            method: "GET"
+            })
+            .then((users)=>{
+                commit('SET_USERS_TO_STATE',users.data)
+                return users
+            })
+            .catch((error)=>
+            {
+                console.log(error)
+                return error
+            })
+        },
+        ADD_USER({commit},user){
+            commit('SET_USER',user);
         }
     },
     getters:{
         BUTTONS(state){
             return state.buttons;
+        },
+        USERS(state){
+            return state.users;
         }
     }
 });
